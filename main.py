1c79050e441a28ad5723d2d43b89bcc4a89036a7
pyvault <twistingnether@protonmail.com>
2016-05-20 15:23:47 +0300

    Stacked DAE.

--- main.py
+++ main.py
@@ -4,29 +4,19 @@ from keras.models import Model
 # this is the size of our encoded representations
 encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
 
-# this is our input placeholder
 input_img = Input(shape=(784,))
-# "encoded" is the encoded representation of the input
-encoded = Dense(encoding_dim, activation='relu')(input_img)
-# "decoded" is the lossy reconstruction of the input
-decoded = Dense(784, activation='sigmoid')(encoded)
+encoded = Dense(128, activation='relu')(input_img)
+encoded = Dense(64, activation='relu')(encoded)
+encoded = Dense(32, activation='relu')(encoded)
 
-# this model maps an input to its reconstruction
+decoded = Dense(64, activation='relu')(encoded)
+decoded = Dense(128, activation='relu')(decoded)
+decoded = Dense(784, activation='sigmoid')(decoded)
+
+
 autoencoder = Model(input=input_img, output=decoded)
-
-
-# this model maps an input to its encoded representation
-encoder = Model(input=input_img, output=encoded)
-
-
-# create a placeholder for an encoded (32-dimensional) input
-encoded_input = Input(shape=(encoding_dim,))
-# retrieve the last layer of the autoencoder model
-decoder_layer = autoencoder.layers[-1]
-# create the decoder model
-decoder = Model(input=encoded_input, output=decoder_layer(encoded_input))
-
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
 
 from keras.datasets import mnist
 import numpy as np
@@ -56,15 +46,15 @@ print x_train.shape
 print x_test.shape
 
 autoencoder.fit(x_train_noisy, x_train,
-                nb_epoch=50,
+                nb_epoch=250,
                 batch_size=256,
                 shuffle=True,
                 validation_data=(x_test_noisy, x_test))
 
 # encode and decode some digits
 # note that we take them from the *test* set
-encoded_imgs = encoder.predict(x_test)
-decoded_imgs = decoder.predict(encoded_imgs)
+
+decoded_imgs = autoencoder.predict(x_test_noisy)
 # use Matplotlib (don't ask)
 import matplotlib.pyplot as plt
 
